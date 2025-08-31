7b290621a41726bd9d2b685b879a63b275886a89
K.V.Jaswanth <kvjaswanth09@gmail.com>
2025-08-31 14:03:51 +0530

    Jash anna1

---
+++ src/script.js
@@ -0,0 +1,43 @@
+// to print on the web page ->
+// document.write("This is 1st step <br><br>")
+
+// document.write(123456)
+
+// document.write("<h1> Hello This is the Heading <H1>")
+
+// var a = 10;
+// ++a
+// var b = 20;
+
+// document.write(a,"<br>" , b)
+
+// var a = 10;
+// var b = 20;
+
+// if(a > b){
+//     document.write ("jash")
+// }
+// else{
+//     document.write ("jashuuu")
+// }
+
+// dialogue boxex :
+
+// 1.alert
+// 2.prompt
+// 3.confirm
+
+// alert("This is Alert");
+
+// prompt("This is Prompt which means text feild")
+// confirm("it should ask the coinfirm msg like are u sure => ok cancel")
+
+// var check = confirm("R u a men??? if u men press cancel  if not press ok")
+// if(check){
+//    name =  prompt("women Please enter the nmae : ")
+//     document.write(name)
+// }
+// else {
+//     name = prompt("men Please enter your name: ")
+//     document.write(name)
+// }
